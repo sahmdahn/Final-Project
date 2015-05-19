548be0e83ebfc204b654ef9c483c058b5191fe9f
MrMann96 <mann.alex.j@HS-324-22.dav.edu>
2015-05-19 08:32:25 -0400

    Alex 5-19-15

--- Final Project/game.h
+++ Final Project/game.h
@@ -17,7 +17,32 @@ public:
 	game();
 	void begin();
 	void direction();
-
+	void HandleMove(int newx, int newy);
 
 };
 
+void game::HandleMove(int newx, int newy){
+	x = newx;
+	y = newy;
+	switch (f[z].Get(x, y)){
+	case '*':
+		return false;
+	case 1:
+		z = 1;
+		break;
+	case 2:
+		z = 2;
+		break;
+	case 3:
+		z = 3;
+		break;
+	case 4:
+		z = 4;
+		break;
+	case 5:
+		z = 5;
+		break;
+	case 'E':
+
+	}
+}
